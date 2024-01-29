95f23744cd7dad1b9286a1c2682f2755e4be8bd0
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-01-29 17:10:10 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petlje4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite pozitivan broj:");
+            int broj = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < broj; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();  
+        }
+    }
+}
